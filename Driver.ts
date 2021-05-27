49afe62e8f9db4b13cae020cdb553ee089d2bae9
Christian <christian@sanglech.com>
2021-05-28 00:16:25 +0900

    Added Rectangle and Driver code

--- Driver.ts
+++ Driver.ts
@@ -1,8 +1,15 @@
 import {Shape} from './Shape';
 import { Circle } from './Circle';
+import { Rectangle } from './Rectangle';
 
 let myShape= new Shape(10,15);
 console.log(myShape.getInfo())
 
 let myCircle = new Circle(5,10,20);
 console.log(myCircle.getInfo());
+
+
+let myRect = new Rectangle(10,10,20,20);
+console.log(myRect.getInfo());
+
+
